ce17fb58615874c7807e3d065e96bf4abd857da3
Camila Silva <camila.medeiros.silva2013@gmail.com>
2024-10-08 19:13:31 +0100

    feat(Header): create a responsive navbar header

---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+import { useContext } from 'react'
+import avatImg from '../../assets/avatar.png'
+import { Link } from 'react-router-dom'
+
+import { AuthContext } from '../../contexts/auth'
+import { FiHome, FiUser, FiSettings } from 'react-icons/fi'
+import './Header.css'
+
+
+export default function Header(){
+  const { user } = useContext(AuthContext)
+  return (
+    <div className="sidebar">
+      <div>
+        <img src={user.avatarUrl === null ? avatImg : user.avatarUrl} alt="User Photo"  />
+      </div>
+      <Link to="/dashboard">
+        <FiHome color='#FFF' size={24} />
+        Callings
+      </Link>
+      <Link to="/customers">
+        <FiUser color='#FFF' size={24} />
+        Clients
+      </Link>
+      <Link to="/profile">
+        <FiSettings color='#FFF' size={24} />
+        Profile
+      </Link>
+    </div>
+
+    
+  )
+}
